564dfb7ea8d31ba3d044c71a3fefeda4d5a36215
vi945786 <87494724+vi945786@users.noreply.github.com>
2024-06-19 01:02:20 +0300

    Delete src/main directory

--- src/main/java/net/vi/mobhealthindicator/render/RenderData.java
+++
@@ -1,8 +0,0 @@
-package net.vi.mobhealthindicator.render;
-
-import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.entity.LivingEntity;
-
-public record RenderData<T extends LivingEntity>(T livingEntity, MatrixStack matrixStack, Renderer renderer, double distance, float maxX, int heartsTotal, int heartsRed, boolean lastRedHalf, int heartsYellow, boolean lastYellowHalf, double heartDensity) {
-
-}
